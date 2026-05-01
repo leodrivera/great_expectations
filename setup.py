426d14a0f6412cb4dc99d03aa03ce17a0e0b0225
Leonardo Rivera <leodrivera@gmail.com>
2026-05-01 13:28:35 -0300

    maint: move redshift to sqla_keys; remove gx-redshift from setup.py extras

--- setup.py
+++ setup.py
@@ -56,7 +56,6 @@ def get_extras_require():
     }
     sqla1x_only_keys = (
         "clickhouse",  # https://github.com/xzkostyan/clickhouse-sqlalchemy/blob/master/setup.py
-        "redshift",  # https://github.com/sqlalchemy-redshift/sqlalchemy-redshift/blob/main/setup.py
         "teradata",  # https://pypi.org/project/teradatasqlalchemy   https://support.teradata.com/knowledge?id=kb_article_view&sys_kb_id=a5a869149729251ced863fe3f153af27
     )
     sqla_keys = (
@@ -71,7 +70,7 @@ def get_extras_require():
         "vertica",  # https://github.com/bluelabsio/sqlalchemy-vertica-python/blob/master/setup.py
         "databricks",  # https://github.com/databricks/databricks-sql-python/blob/main/pyproject.toml
         "snowflake",  # https://github.com/snowflakedb/snowflake-sqlalchemy/blob/main/setup.cfg
-        "gx-redshift",  # https://github.com/great-expectations/sqlalchemy-redshift/blob/main/setup.py
+        "redshift",  # https://github.com/sqlalchemy-redshift/sqlalchemy-redshift/blob/main/setup.py
     )
     ignore_keys = (
         "sqlalchemy",
